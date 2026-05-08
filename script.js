eff9b2420d55aabf83d3b08c2cde28c8fc00f310
rithikrao4141-commits <rithikrao4141@gmail.com>
2026-05-08 14:57:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+<script>
+ fuction showmenu(){
+
+   let menu=
+document=getElementById("menu");
+
+ 
+if( menu.style.display==="block"){
+ menu.style.display="none"
+}
+
+    else{
+ menu.style.display= "block";
+}
+
+  </script>
+
