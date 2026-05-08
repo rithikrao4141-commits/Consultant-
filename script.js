07da0593d1821241d4dafe14510951558c5578fc
rithikrao4141-commits <rithikrao4141@gmail.com>
2026-05-08 23:30:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,15 @@
 document.getElementById("menu");
 
  
-if( menu.style.display==="block"){
+if( menu.style.display==="flex"){
+
  menu.style.display="none"
 }
 
     else{
- menu.style.display= "block";}
+ menu.style.display= "flex";
+   
+  }
 
 
 }
